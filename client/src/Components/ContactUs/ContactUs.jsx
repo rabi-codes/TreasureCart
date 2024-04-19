95a47c6b0e4dd5e3fff4a130507e082593ef4639
rabi-codes <rabeehadetomi@gmail.com>
2024-04-19 13:20:00 +0100

    contact page amendments

--- client/src/Components/ContactUs/ContactUs.jsx
+++ client/src/Components/ContactUs/ContactUs.jsx
@@ -7,7 +7,7 @@ import backgroundImage from "./image/footerBackground.png";
 function ContactUs() {
   return (
     <div className="mainContainer">
-      <div className="map">
+      <div className="map cursor-pointer">
         <img src={mapImage} alt="" />
       </div>
       <div className="getInTouch">
@@ -15,7 +15,7 @@ function ContactUs() {
           Get In Touch With Us
         </h1>
 
-        <hr className=" shortLine mb-8 h-[1.5px] w-16 bg-[#fbb331] border-none" />
+        <hr className=" shortLine mb-8 -mt-[10px] h-[1.4px] w-16 bg-[#fbb331] border-none" />
 
         <p className=" flex justify-center items centre mb-12 font-body text-[#979797]">
           We are here to help. Want to learn more about our services? Please get
@@ -25,14 +25,14 @@ function ContactUs() {
       <div className=" middle flex items-center justify-center">
         <div className="inputForm">
           <form>
-            <div className="mt-10 mr-[8rem]">
+            <div className="mt-10 mr-[6rem]">
               <div className="userName">
                 <input
                   type="text"
                   name="first-name"
                   id="first-name"
                   placeholder="Name"
-                  className="p-4 w-[400px] mb-[30px] border-2"
+                  className="p-4 w-[450px] mb-[35px] border-2"
                 />
               </div>
               <div className="userMail">
@@ -41,7 +41,7 @@ function ContactUs() {
                   name="email"
                   id="email"
                   placeholder="Email"
-                  className="p-4 w-[400px] mb-[30px] border-2"
+                  className="p-4 w-[450px] mb-[35px] border-2"
                 />
               </div>
               <div className="userNumber">
@@ -50,7 +50,7 @@ function ContactUs() {
                   name="phone-number"
                   id="phone-number"
                   placeholder="Phone"
-                  className="p-4 w-[400px] mb-[30px] border-2"
+                  className="p-4 w-[450px] mb-[35px] border-2"
                 />
               </div>
               <div className="messageUs">
@@ -59,18 +59,18 @@ function ContactUs() {
                   name="message"
                   id="message"
                   placeholder="Message"
-                  className=" p-4 w-[400px] h-[120px] mb-[30px] border-2 "
+                  className=" p-4 w-[450px] h-[120px] mb-[35px] border-2 "
                 />
               </div>
-              <button className=" p-4 w-56 rounded bg-[#ff971f] text-white border-none font-body">
+              <button className=" p-4 w-56 rounded bg-[#ff971f] text-white border-none font-body cursor-pointer hover:bg-[#ff6600]/[.8] ">
                 SEND MESSAGE
               </button>
             </div>
           </form>
         </div>
 
-        <div className=" right p-12 pb-[50px] -mt-[10rem] h-[200px] w-[300px] border-dashed border-black/[.3] border-1">
-          <h2 className="font-body -mt-[15px] font-thin ">HEADQUATER</h2>
+        <div className=" right p-12 pb-[58px] -mt-[9.5rem] h-[200px] w-[270px] border-dashed border-black/[.3] border-1">
+          <h2 className="font-body -mt-[18px] font-thin ">HEADQUATER</h2>
           <div className=" findus mb-8 location flex items-center font-body">
             <div className=" locationIcon text-[#ff971f]">
               <FaLocationPin />
@@ -89,36 +89,36 @@ function ContactUs() {
             <div className=" envelopeIcon text-[#ff971f]">
               <FaEnvelope />
             </div>
-            <p className="ml-4 text-[#55acee] underline underline-offset-1  ">
+            <p className="ml-4 text-[#55acee] underline underline-offset-1 cursor-pointer ">
               hitreasurecart@gmail.com
             </p>
           </div>
         </div>
       </div>
-      <div className=" bottom ">
-        {/* <div className="bottomImage opacity-25 h-[150px]">
-          <img className="object-fill w-[100%]" src={backgroundImage} alt="" />
-        </div> */}
-        {/* <div className="bottomImage opacity-25 h-[150px]">
+
+      <div className="bottom relative mt-16 ">
+        <div className="bottomImage opacity-[.10]">
           <img
             className="object-fill w-full h-full"
             src={backgroundImage}
             alt=""
           />
-        </div> */}
-        <div className=" flex text-center items-center justify-center gap-44 bg-[#eff2ec]/[.6] h-40 mt-16 font-body z-0">
-          <div className="bottomLeft">
-            <h2 className="text-[#fbb331] font-thin">
-              Do You Like Treasure Cart? Download it Now
-            </h2>
-            <p className="-mt-4 font-body text-[#979797] ">
-              Available on playstore and AppStore
-            </p>
-          </div>
-          <div className="bottomRight">
-            <button className="p-[12px] rounded w-68 bg-[#ff971f] -mt-8 text-white border-none font-body">
-              DOWNLOAD NOW! - FOREVER FREE
-            </button>
+        </div>
+        <div className="downloadApp absolute inset-0">
+          <div className="flex items-center justify-center gap-56 mt-14 font-body">
+            <div className="bottomLeft text-center">
+              <h2 className="text-[#fbb331] font-thin">
+                Do You Like Treasure Cart? Download it Now
+              </h2>
+              <p className=" -mt-4 font-body text-[#979797]">
+                Available on Play Store and App Store
+              </p>
+            </div>
+            <div className="bottomButton">
+              <button className="p-[15px] mt-[12px] rounded w-68 bg-[#ff971f] text-white border-none font-body cursor-pointer hover:bg-[#ff6600]/[.8]">
+                DOWNLOAD NOW! - FOREVER FREE
+              </button>
+            </div>
           </div>
         </div>
       </div>
